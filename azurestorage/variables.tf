7bae45ef1f09074a3ea969853dd4c8390d99b470
michel.mutabesha@cgi.com <michel.mutabesha@cgi.com>
2023-12-13 12:00:25 -0500

    added storage module.

---
+++ azurestorage/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "eatus-storage-rg"
+}
+variable "resource_group_location" {
+  default = "East US"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "storage_account_name" {
+  default = "eatusstoragest"
+}
+variable "storage_container_name" {
+  default = "eatusstoragesc"
+}
